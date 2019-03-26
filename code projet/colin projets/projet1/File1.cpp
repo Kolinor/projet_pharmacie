7182e4f670e2f210f155a91582d14723f9ed356f
Colin <colin.mangas@gmail.com>
2019-03-26 17:46:09 +0100

    colin tcp serv

--- code projet/colin projets/projet1/File1.cpp
+++ code projet/colin projets/projet1/File1.cpp
@@ -3,6 +3,8 @@
 
 #ifdef _WIN32
 #include <tchar.h>
+#include <iostream>
+#include <string.h>
 #else
   typedef char _TCHAR;
   #define _tmain main
@@ -13,7 +15,15 @@
 
  int _tmain(int argc, _TCHAR* argv[]) 
 {
+	tcpClient * tcp;
+	tcp = new(tcpClient);
 
+	tcp->connected("192.168.65.7", 9013);
+	tcp->sendString("METEO\n");
+	string coucou = tcp->readString();
+	cout << coucou << endl;
+
+	system("pause");
 
 	return 0;
 }
