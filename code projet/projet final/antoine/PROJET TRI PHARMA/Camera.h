c75341b396cb655b2867fef29321eb695730599a
Kolinor <30359450+Kolinor@users.noreply.github.com>
2019-04-29 16:02:59 +0200

    projet final 1.0

---
+++ code projet/projet final/antoine/PROJET TRI PHARMA/Camera.h
@@ -0,0 +1,17 @@
+//---------------------------------------------------------------------------
+
+#ifndef CameraH
+#define CameraH
+//---------------------------------------------------------------------------
+
+class Camera {
+
+private:
+	float longueur,largeur,hauteur;
+public:
+
+	void mesurerBoiteHauteur();
+	void mesurerBoiteLongueur();
+	void mesurerBoiteLargeur();
+};
+#endif
