ff88408068798782d12ab65ca294cf16db19487d
KVuillemenot <46051118+KVuillemenot@users.noreply.github.com>
2019-06-12 14:49:16 +0200

    maj

--- CODE PROJET FINAL/include/MysqlPharmacieManager/MysqlPharmacieManager.cpp
+++ CODE PROJET FINAL/include/MysqlPharmacieManager/MysqlPharmacieManager.cpp
@@ -110,6 +110,94 @@ bool MysqlPharmacieManager::insertTapiris(String cle,String value)
 }
 //---------------------------------------------------------------------------
 
+bool MysqlPharmacieManager::changerEtat(String etat, String id_ordonnance)
+{
+	String requete = "UPDATE `Ordonnance` SET `Etat` = '";
+	requete += etat.c_str();
+	requete += "' WHERE `ID_Ordonnance` = ";
+	requete += id_ordonnance.c_str();
+
+	wchar_t * wStrReq = requete.c_str();
+	int reqLength = wcslen(wStrReq);
+	char * req = new char[reqLength +1];
+	wcstombs(req,wStrReq, reqLength);
+	this->mySql->update(req);
+}
+//---------------------------------------------------------------------------
+
+int MysqlPharmacieManager::selectCaisse(String codeBarre)
+{
+	vector < vector<string> > repCaisse;
+	vector < vector<string> > repOrdo;
+
+	String requete = "SELECT `Ordonnance`.`Numero_Caisse`, `Ordonnance`.`ID_Ordonnance`, `Ordonnance_Medicament_Association`.`Nom_Medicament`";
+	requete += " FROM `Ordonnance` INNER JOIN `Ordonnance_Medicament_Association` ON";
+	requete += "`Ordonnance`.`ID_Ordonnance` = `Ordonnance_Medicament_Association`.`ID_Ordonnance`";
+	requete += "INNER JOIN `Medicament` ON `Medicament`.`Nom_Medicament` = `Ordonnance_Medicament_Association`.`Nom_Medicament`";
+	requete += "WHERE `Medicament`.`Code_Barre` =";
+	requete += codeBarre;
+	requete += " AND `Ordonnance_Medicament_Association`.`Quantite_Delivree` <";
+	requete += "`Ordonnance_Medicament_Association`.`Quantite_Demande` AND";
+	requete += "`Ordonnance`.`Etat` = 'en attente' order by `Ordonnance`.`Date_Saisie_Ordonnance` asc limit 1;";
+
+	wchar_t * wStrReq = requete.c_str();
+	int reqLength = wcslen(wStrReq);
+	char * req = new char[reqLength +1];
+	wcstombs(req,wStrReq, reqLength);
+	repCaisse = this->mySql->select(req);
+
+	string id;
+	string medicament;
+	int caisse;
+
+	if (repCaisse.size() != 0)
+	{
+		caisse = atoi(repCaisse[0][0].c_str());
+		id = repCaisse[0][1];
+		medicament = repCaisse[0][2];
+
+		String requete1 = "UPDATE `Ordonnance_Medicament_Association` SET `Quantite_Delivree`= Quantite_Delivree+1 ";
+		requete1 += "WHERE `Quantite_Demande` > `Quantite_Delivree` ";
+		requete1 += "AND `Ordonnance_Medicament_Association`.`ID_Ordonnance` = ";
+		requete1 += id.c_str();
+		requete1 += " AND `Ordonnance_Medicament_Association`.`Nom_Medicament` = '";
+		requete1 += medicament.c_str();
+		requete1 += "'";
+		wchar_t * wStrReq1 = requete1.c_str();
+		int reqLength1 = wcslen(wStrReq1);
+		char * req1 = new char[reqLength1 +1];
+		wcstombs(req1,wStrReq1, reqLength1);
+		this->mySql->update(req1);
+
+		String requete2 = "SELECT `Ordonnance_Medicament_Association`.`ID_Ordonnance`, `Ordonnance_Medicament_Association`.`Quantite_Demande`, `Ordonnance_Medicament_Association`.`Quantite_Delivree` FROM `Ordonnance` ";
+		requete2 += "INNER JOIN `Ordonnance_Medicament_Association` ON `Ordonnance`.`ID_Ordonnance` = `Ordonnance_Medicament_Association`.`ID_Ordonnance` ";
+		requete2 += "INNER JOIN `Medicament` ON `Medicament`.`Nom_Medicament` = `Ordonnance_Medicament_Association`.`Nom_Medicament` ";
+		requete2 += "WHERE `Ordonnance_Medicament_Association`.`Quantite_Delivree` < `Ordonnance_Medicament_Association`.`Quantite_Demande` ";
+		requete2 += "AND `Ordonnance_Medicament_Association`.`ID_Ordonnance` = ";
+		requete2 += id.c_str();
+
+		wchar_t * wStrReq2 = requete2.c_str();
+		int reqLength2 = wcslen(wStrReq2);
+		char * req2 = new char[reqLength2 +1];
+		wcstombs(req2,wStrReq2, reqLength2);
+		repOrdo = this->mySql->select(req2);
+
+		if (repOrdo.size() == 0 )
+		{
+			string etat = "terminer";
+			this->changerEtat(etat.c_str(),id.c_str());
+		}
+	}
+
+	else
+	{
+		caisse = 3;
+	}
+
+	return caisse;
+}
+//---------------------------------------------------------------------------
+
 vector < vector<string> > MysqlPharmacieManager::BilanAnnee()
 {
 	vector < vector<string> > bilan;
@@ -283,90 +371,3 @@ vector < vector<string> > MysqlPharmacieManager::BilanJourMedicament(String medi
 }
 //---------------------------------------------------------------------------
 
-bool MysqlPharmacieManager::changerEtat(String etat, String id_ordonnance)
-{
-	String requete = "UPDATE `Ordonnance` SET `Etat` = '";
-	requete += etat.c_str();
-	requete += "' WHERE `ID_Ordonnance` = ";
-	requete += id_ordonnance.c_str();
-
-	wchar_t * wStrReq = requete.c_str();
-	int reqLength = wcslen(wStrReq);
-	char * req = new char[reqLength +1];
-	wcstombs(req,wStrReq, reqLength);
-	this->mySql->update(req);
-}
-//---------------------------------------------------------------------------
-
-int MysqlPharmacieManager::selectCaisse(String codeBarre)
-{
-	vector < vector<string> > repCaisse;
-	vector < vector<string> > repOrdo;
-
-	String requete = "SELECT `Ordonnance`.`Numero_Caisse`, `Ordonnance`.`ID_Ordonnance`, `Ordonnance_Medicament_Association`.`Nom_Medicament`";
-	requete += " FROM `Ordonnance` INNER JOIN `Ordonnance_Medicament_Association` ON";
-	requete += "`Ordonnance`.`ID_Ordonnance` = `Ordonnance_Medicament_Association`.`ID_Ordonnance`";
-	requete += "INNER JOIN `Medicament` ON `Medicament`.`Nom_Medicament` = `Ordonnance_Medicament_Association`.`Nom_Medicament`";
-	requete += "WHERE `Medicament`.`Code_Barre` =";
-	requete += codeBarre;
-	requete += " AND `Ordonnance_Medicament_Association`.`Quantite_Delivree` <";
-	requete += "`Ordonnance_Medicament_Association`.`Quantite_Demande` AND";
-	requete += "`Ordonnance`.`Etat` = 'en attente' order by `Ordonnance`.`Date_Saisie_Ordonnance` asc limit 1;";
-
-	wchar_t * wStrReq = requete.c_str();
-	int reqLength = wcslen(wStrReq);
-	char * req = new char[reqLength +1];
-	wcstombs(req,wStrReq, reqLength);
-	repCaisse = this->mySql->select(req);
-
-	string id;
-	string medicament;
-	int caisse;
-
-	if (repCaisse.size() != 0)
-	{
-		caisse = atoi(repCaisse[0][0].c_str());
-		id = repCaisse[0][1];
-		medicament = repCaisse[0][2];
-
-		String requete1 = "UPDATE `Ordonnance_Medicament_Association` SET `Quantite_Delivree`= Quantite_Delivree+1 ";
-		requete1 += "WHERE `Quantite_Demande` > `Quantite_Delivree` ";
-		requete1 += "AND `Ordonnance_Medicament_Association`.`ID_Ordonnance` = ";
-		requete1 += id.c_str();
-		requete1 += " AND `Ordonnance_Medicament_Association`.`Nom_Medicament` = '";
-		requete1 += medicament.c_str();
-		requete1 += "'";
-		wchar_t * wStrReq1 = requete1.c_str();
-		int reqLength1 = wcslen(wStrReq1);
-		char * req1 = new char[reqLength1 +1];
-		wcstombs(req1,wStrReq1, reqLength1);
-		this->mySql->update(req1);
-
-		String requete2 = "SELECT `Ordonnance_Medicament_Association`.`ID_Ordonnance`, `Ordonnance_Medicament_Association`.`Quantite_Demande`, `Ordonnance_Medicament_Association`.`Quantite_Delivree` FROM `Ordonnance` ";
-		requete2 += "INNER JOIN `Ordonnance_Medicament_Association` ON `Ordonnance`.`ID_Ordonnance` = `Ordonnance_Medicament_Association`.`ID_Ordonnance` ";
-		requete2 += "INNER JOIN `Medicament` ON `Medicament`.`Nom_Medicament` = `Ordonnance_Medicament_Association`.`Nom_Medicament` ";
-		requete2 += "WHERE `Ordonnance_Medicament_Association`.`Quantite_Delivree` < `Ordonnance_Medicament_Association`.`Quantite_Demande` ";
-		requete2 += "AND `Ordonnance_Medicament_Association`.`ID_Ordonnance` = ";
-		requete2 += id.c_str();
-
-		wchar_t * wStrReq2 = requete2.c_str();
-		int reqLength2 = wcslen(wStrReq2);
-		char * req2 = new char[reqLength2 +1];
-		wcstombs(req2,wStrReq2, reqLength2);
-		repOrdo = this->mySql->select(req2);
-
-		if (repOrdo.size() == 0 )
-		{
-			string etat = "terminer";
-			this->changerEtat(etat.c_str(),id.c_str());
-		}
-	}
-
-	else
-	{
-		caisse = 3;
-	}
-
-	return caisse;
-}
-//---------------------------------------------------------------------------
